405aa2ac5fdf1f4a8ad54490278a3cdadb234e5e
Parasnaveen <flutterintern@parastechnologies.com>
2025-03-17 16:41:52 +0530

    .

--- lib/feature/auth/presentation/provider/sign_up_provider.dart
+++ lib/feature/auth/presentation/provider/sign_up_provider.dart
@@ -2,6 +2,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zrayo_flutter/feature/auth/data/data_source/auth_data_source.dart';
 import 'package:zrayo_flutter/feature/auth/data/repositories/auth_repo_implementation.dart';
+import 'package:zrayo_flutter/feature/auth/presentation/provider/state_notifiers/sign_up_notifier.dart';
 import 'package:zrayo_flutter/feature/auth/presentation/provider/states/auth_states.dart';
 
 final authDataProvider =
