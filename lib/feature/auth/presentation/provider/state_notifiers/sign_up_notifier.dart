36278de3435028fe8059b2eb20cc268eeaf2dafb
Rahul Chopra <Gurpyarsingh@parastechnologies.com>
2025-03-17 16:41:20 +0530

    merge

--- lib/feature/auth/presentation/provider/state_notifiers/sign_up_notifier.dart
+++ lib/feature/auth/presentation/provider/state_notifiers/sign_up_notifier.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zrayo_flutter/feature/auth/data/repositories/auth_repo_implementation.dart';
-import 'package:zrayo_flutter/feature/auth/presentation/provider/states/auth_states.dart';
+import 'package:zrayo_flutter/feature/auth/presentation/provider/states/login_states.dart';
 
 class SignUpNotifier extends StateNotifier<SignUpState> {
   final AuthRepository authRepo;
