1bcc813c210e3e9caffdff5c99ed3ce5e13160e4
Parasnaveen <flutterintern@parastechnologies.com>
2025-04-08 16:42:14 +0530

    rendering issue in subscription screen

--- lib/feature/auth/presentation/views/create_profile_steps/subscription_plan.dart
+++ lib/feature/auth/presentation/views/create_profile_steps/subscription_plan.dart
@@ -17,81 +17,85 @@ class SubscriptionPlanView extends StatelessWidget {
         body: Column(spacing: 5.sp, children: [
       CustomAppBar(title: ''),
       yHeight(context.height / 15),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AppText(
-              text: AppString.choosePlan,
-              textSize: 24.sp,
-              fontFamily: AppFonts.satoshiBold,
-            ),
-            yHeight(5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text.rich(
-                      TextSpan(
-                        text: AppString.getOneMonth,
-                        style: TextStyle(
-                            fontSize: 14.sp,
-                            fontFamily: AppFonts.satoshiRegular),
-                        children: [
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AppText(
+                  text: AppString.choosePlan,
+                  textSize: 24.sp,
+                  fontFamily: AppFonts.satoshiBold,
+                ),
+                yHeight(5),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text.rich(
                           TextSpan(
-                            text: AppString.freeTrial,
+                            text: AppString.getOneMonth,
                             style: TextStyle(
                                 fontSize: 14.sp,
-                                fontFamily: AppFonts.satoshiBold),
+                                fontFamily: AppFonts.satoshiRegular),
+                            children: [
+                              TextSpan(
+                                text: AppString.freeTrial,
+                                style: TextStyle(
+                                    fontSize: 14.sp,
+                                    fontFamily: AppFonts.satoshiBold),
+                              ),
+                              TextSpan(
+                                text: AppString.freeTrialEnds,
+                                style: TextStyle(
+                                  fontFamily: AppFonts.satoshiRegular,
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                            ],
                           ),
-                          TextSpan(
-                            text: AppString.freeTrialEnds,
-                            style: TextStyle(
-                              fontFamily: AppFonts.satoshiRegular,
-                              fontSize: 14.sp,
-                            ),
-                          ),
-                        ],
+                          textAlign: TextAlign.center,
+                          softWrap: true,
+                          overflow: TextOverflow.visible,
+                        ),
                       ),
-                      textAlign: TextAlign.center,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            yHeight(30),
-            Column(
-              spacing: 15.sp,
-              children: [
-                PlanCard(
-                  title: AppString.free,
-                  price: AppString.price0,
-                  description: AppString.givesDesignatedContact,
-                  onTap: () {},
                 ),
-                PlanCard(
-                  isSelected: true,
-                  title: AppString.basic,
-                  price: AppString.priceAbove0,
-                  description: AppString.accessToBasiscTabs,
-                  isHighlighted: true,
-                  trialText: AppString.free7DaysTrial,
-                  onTap: () {},
+                yHeight(30),
+                Column(
+                  spacing: 15.sp,
+                  children: [
+                    PlanCard(
+                      title: AppString.free,
+                      price: AppString.price0,
+                      description: AppString.givesDesignatedContact,
+                      onTap: () {},
+                    ),
+                    PlanCard(
+                      isSelected: true,
+                      title: AppString.basic,
+                      price: AppString.priceAbove0,
+                      description: AppString.accessToBasiscTabs,
+                      isHighlighted: true,
+                      trialText: AppString.free7DaysTrial,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+                yHeight(context.height / 7),
+                CommonAppBtn(
+                  title: AppString.continueText,
+                  onTap: () => fromSettings
+                      ? back(context)
+                      : offAllNamed(context, Routes.dashboard),
                 ),
               ],
             ),
-            yHeight(context.height / 7),
-            CommonAppBtn(
-              title: AppString.continueText,
-              onTap: () => fromSettings
-                  ? back(context)
-                  : offAllNamed(context, Routes.dashboard),
-            ),
-          ],
+          ),
         ),
       ),
     ]));
