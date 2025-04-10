5b7764277d05ecab2555e5d5bb058b8f5992b1ad
Rahul Chopra <Gurpyarsingh@parastechnologies.com>
2025-04-10 11:55:08 +0530

    chat screen design changed

--- lib/feature/messages/presentation/view/chat_tile.dart
+++ lib/feature/messages/presentation/view/chat_tile.dart
@@ -30,83 +30,96 @@ class ChatTile extends StatelessWidget {
         margin: EdgeInsets.symmetric(vertical: context.width * .02),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  CustomCacheNetworkImage(
-                    img: "",
-                    size: 50,
-                  ),
-                  10.horizontalSpace,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: AppText(
-                                text: name,
-                                textSize: 16.sp,
-                                fontFamily: AppFonts.satoshiBold,
-                                maxlines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ),
-                        8.verticalSpace,
-                        Row(
-                          children: [
-                            Expanded(
-                              child: AppText(
-                                  text: message,
-                                  textSize: 14.sp,
-                                  maxlines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  color: unreadCount > 0
-                                      ? AppColor.black000000
-                                      : AppColor.grey999999),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      AppText(
-                        text: time,
-                        textSize: 12.sp,
-                        fontFamily: AppFonts.satoshiRegular,
-                        color: AppColor.greyAAA6B9,
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: CustomCacheNetworkImage(
+                        img: "",
+                        size: 55,
                       ),
-                      if (unreadCount > 0)
-                        Container(
-                          padding: const EdgeInsets.all(8.0),
-                          margin: const EdgeInsets.only(top: 4),
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: AppColor.primary,
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: CustomCacheNetworkImage(
+                        img: "",
+                        size: 33,
+                      ),
+                    )
+                  ],
+                ),
+                10.horizontalSpace,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: AppText(
+                              text: name,
+                              textSize: 16.sp,
+                              fontFamily: AppFonts.satoshiBold,
+                              maxlines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
-                          child: AppText(
-                            text: unreadCount.toString(),
-                            textSize: 10,
+                        ],
+                      ),
+                      8.verticalSpace,
+                      Row(
+                        children: [
+                          Expanded(
+                            child: AppText(
+                                text: message,
+                                textSize: 14.sp,
+                                maxlines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                color: unreadCount > 0
+                                    ? AppColor.black000000
+                                    : AppColor.grey999999),
                           ),
-                        ),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    AppText(
+                      text: time,
+                      textSize: 12.sp,
+                      fontFamily: AppFonts.satoshiRegular,
+                      color: AppColor.greyAAA6B9,
+                    ),
+                    if (unreadCount > 0)
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        margin: const EdgeInsets.only(top: 4),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColor.primary,
+                        ),
+                        child: AppText(
+                          text: unreadCount.toString(),
+                          textSize: 10,
+                        ),
+                      ),
+                  ],
+                ),
+              ],
             ),
+            yHeight(context.width * .03),
             Divider(
-              thickness: 1.sp,
               color: AppColor.black000000.withValues(alpha: .1),
+              height: 0,
             )
           ],
         ),
